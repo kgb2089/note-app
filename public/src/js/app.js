8862e8d1c1885cef38a7d829ffe4e338314aff8a
Gwangbum <kgb2089@naver.com>
2017-12-19 23:08:51 +0900

    comm

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -61,7 +61,7 @@ var saveAs = saveAs || (function(view) {
 			var revoker = function() {
 				if (typeof file === "string") {
 					get_URL().revokeObjectURL(file);
-				} else 
+				} else {
 					file.remove();
 				}
 			};
@@ -106,7 +106,7 @@ var saveAs = saveAs || (function(view) {
 							var url = is_chrome_ios ? reader.result : reader.result.replace(/^data:[^;]*;/, 'data:attachment/file;');
 							var popup = view.open(url, '_blank');
 							if(!popup) view.location.href = url;
-							url=undefined; // release reference before dispatching
+							url=undefined;
 							filesaver.readyState = filesaver.DONE;
 							dispatch_all();
 						};
@@ -151,7 +151,38 @@ var saveAs = saveAs || (function(view) {
 		, saveAs = function(blob, name, no_auto_bom) {
 			return new FileSaver(blob, name || blob.name || "Memo", no_auto_bom);
 		}
-	);
+	;
+	if (typeof navigator !== "undefined" && navigator.msSaveOrOpenBlob) {
+		return function(blob, name, no_auto_bom) {
+			name = name || blob.name || "Memo";
+
+			if (!no_auto_bom) {
+				blob = auto_bom(blob);
+			}
+			return navigator.msSaveOrOpenBlob(blob, name);
+		};
+	}
+
+	FS_proto.abort = function(){};
+	FS_proto.readyState = FS_proto.INIT = 0;
+	FS_proto.WRITING = 1;
+	FS_proto.DONE = 2;
+
+	FS_proto.error =
+	FS_proto.onwritestart =
+	FS_proto.onprogress =
+	FS_proto.onwrite =
+	FS_proto.onabort =
+	FS_proto.onerror =
+	FS_proto.onwriteend =
+		null;
+
+	return saveAs;
+}(
+	   typeof self !== "undefined" && self
+	|| typeof window !== "undefined" && window
+	|| this.content
+));
 
 function doesFileExist(url) {
     var fileObject=new ActiveXObject("Scripting.FileSystemObject");
